5634206d030056691d10f014ad679997f1d55cf8
Sanit Roy <sanitroy2000@gmail.com>
2021-06-18 11:53:16 +0530

    added basic routes and dummy pages

--- app.js
+++ app.js
@@ -8,7 +8,17 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home');
-})
+});
+app.get('/about', (req, res) => {
+    res.render('sections/about');
+});
+app.get('/events', (req, res) => {
+    res.render('sections/events');
+});
+app.get('/contact', (req, res) => {
+    res.render('sections/contact');
+});
+
 
 app.listen(3000, () => {
     console.log('serving on port 3000');
